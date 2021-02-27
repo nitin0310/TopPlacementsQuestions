c03c9f673c455eca57ee6cccab8fae0b5c956bf6
Nitin <45264530+nitin0310@users.noreply.github.com>
2021-02-27 16:28:50 +0530

    Update Question5.java

--- SlidingWindowPrblms/src/variableSizeWindow/Question5.java
+++ SlidingWindowPrblms/src/variableSizeWindow/Question5.java
@@ -2,39 +2,54 @@ package variableSizeWindow;
 
 public class Question5 {
 	public static void main(String args[]) {
+		String string="TOTMTAOA";
+		String sub="TTA";
 		
+		System.out.println(minimumWindowSubstring(string.toCharArray(),sub.toCharArray()));
 	}
 	
 	static int minimumWindowSubstring(char[] string,char[] sub) {
 		
-		int[] subCharCount=new int[256];
-		int[] stringCharCount=new int[256];
+		HashMap<Character,Integer> subCharCount=new HashMap<Character,Integer>();
 		
 		int n=string.length;
 		int start=0;
 		int minLength=Integer.MAX_VALUE;
 		
-		for(int i=0;i<sub.length;i++)	subCharCount[sub[i]]++;
-		
+		for(int i=0;i<sub.length;i++) {
+			if(subCharCount.containsKey(sub[i])) {
+				subCharCount.put(sub[i], subCharCount.get(sub[i])+1);
+			}else{
+				subCharCount.put(sub[i], 1);
+			}
+		}
+		int uniqueItemCount=subCharCount.size();
 		
 		for(int end=0;end<n;end++) {
-			stringCharCount[string[end]]++;
+			if(subCharCount.containsKey(string[end])) {
+				subCharCount.put(string[end], subCharCount.get(string[end])-1);
+				if(subCharCount.get(string[end])==0) {
+					uniqueItemCount--;
+				}
+			}
 			
-			if(isSubFoundValid(subCharCount,stringCharCount,sub)) {
+			while(uniqueItemCount==0) {
 				if(end-start+1<minLength) {
 					minLength=end-start+1;
 				}
+				
+				if(subCharCount.containsKey(string[start])){
+					subCharCount.put(string[start], subCharCount.get(string[start])+1);
+					if(subCharCount.get(string[start])==1) {
+						uniqueItemCount++;
+					}
+				}
+				
+				start++;
 			}
-		}
-	}
-	
-	static boolean isSubFoundValid(int[] subCharCount,int[] stringCharCount,char[] sub) {
-		for(int i=0;i<sub.length;i++) {
-			if(stringCharCount[sub[i]]<subCharCount[sub[i]]) {
-				return false;
-			}
+			
 		}
 		
-		return true;
+		return minLength;
 	}
 }
